f7b3a1222ff8ede7b25d6b98b4926bc2f205ea1b
silentvoice143 <silentvoice4143@gmail.com>
2026-05-20 01:35:30 +0530

    feat: implemented basic spec and public api designs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,32 @@
+import React, { useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'rn-event-log';
 
-const result = multiply(3, 7);
+import Analytics from 'rn-event-log';
 
 export default function App() {
+  useEffect(() => {
+    Analytics.init({
+      debug: true,
+      flushInterval: 30,
+    });
+
+    Analytics.startSession();
+
+    Analytics.track('app_opened', {
+      screen: 'Home',
+      platform: 'android',
+    });
+
+    Analytics.flush();
+
+    return () => {
+      Analytics.closeSession();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>RN Event Log Working 🚀</Text>
     </View>
   );
 }
